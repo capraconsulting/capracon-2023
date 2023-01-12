789f0647f170a1319aa8203f925a01ca994aab35
Camilla <camillamdalan@gmail.com>
2023-01-12 19:51:57 +0100

    :wrench: add root capracon database

--- app/config.ts
+++ app/config.ts
@@ -2,4 +2,5 @@ export const config = {
   conferenceId: "46d596b83b73401ea71c9dca4b6a3818",
   personsDatabaseId: "0142b66ca341477b982ff8301e083dbc",
   talksDatabaseId: "24bb721180114ffea2a0d0f405985533",
+  masterProgramDatabaseId: "b0368d7a74f14679beb4991b2a860dc1",
 } as const;
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -4,35 +4,18 @@ import { useLoaderData } from "@remix-run/react";
 
 import { config } from "~/config";
 import { getClient } from "~/notion/notion-remix-cloudflare";
-import {
-  parseConference,
-  parsePerson,
-  parseTalk,
-  parseTimeslots,
-  parseTracks,
-} from "~/notion-conference/domain";
 import { getEnvVariableOrThrow } from "~/utils/env";
 
 export const loader = async ({ context }: LoaderArgs) => {
   const token = getEnvVariableOrThrow("NOTION_TOKEN", context);
 
   // Fetch
-  const [notionConference, notionTalksDatabase, notionTalks, notionPersons] =
-    await Promise.all([
-      getClient(token).getPage(config.conferenceId),
-      getClient(token).getDatabase(config.talksDatabaseId),
-      getClient(token).getDatabasePages(config.talksDatabaseId),
-      getClient(token).getDatabasePages(config.personsDatabaseId),
-    ]);
+  const [masterProgram] = await Promise.all([
+    getClient(token).getDatabasePages(config.masterProgramDatabaseId),
+  ]);
 
-  const persons = notionPersons.map(parsePerson);
-  const talks = notionTalks.map((x) => parseTalk(x, persons));
   return json({
-    conference: parseConference(notionConference),
-    persons,
-    talks,
-    tracks: parseTracks(notionTalksDatabase),
-    timeslots: parseTimeslots(notionTalksDatabase),
+    masterProgram,
   });
 };
 
