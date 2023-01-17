99d665a6346e52801497ecf27ffc05ba9ff7bfd9
Julian Jark <juliuxu@gmail.com>
2023-01-17 21:07:59 +0100

    :sparkles: use text from notion on kontakt and praktisk pages

--- app/routes/kontakt.tsx
+++ app/routes/kontakt.tsx
@@ -1,15 +1,26 @@
+import type { MetaFunction } from "@remix-run/cloudflare";
+
 import { ContentBox } from "~/components/content-box";
 import { Title } from "~/components/title";
+import type { RootLoader } from "~/root";
+import { useRootData } from "~/root";
+
+export const meta: MetaFunction<never, { root: RootLoader }> = ({
+  parentsData,
+}) => ({
+  title: parentsData["root"].conference.kontaktTitle,
+  description: parentsData["root"].conference.kontaktDescription,
+});
 
 export default function Kontakt() {
+  const data = useRootData();
+
   return (
     <ContentBox>
       <Title as="h1" withBackground size="text-6xl">
-        Kontakt
+        {data.conference.kontaktTitle}
       </Title>
-      <p className="mb-12 text-3xl">
-        Har du spørsmål? Ta kontakt med en av oss!
-      </p>
+      <p className="mb-12 text-3xl">{data.conference.kontaktDescription}</p>
     </ContentBox>
   );
 }
--- app/routes/praktisk.tsx
+++ app/routes/praktisk.tsx
@@ -1,12 +1,45 @@
+import type { MetaFunction } from "@remix-run/cloudflare";
+
+import { ContentBox } from "~/components/content-box";
 import { Title } from "~/components/title";
+import type { RootLoader } from "~/root";
+import { useRootData } from "~/root";
+
+export const meta: MetaFunction<never, { root: RootLoader }> = ({
+  parentsData,
+}) => ({
+  title: parentsData["root"].conference.praktiskTitle,
+  description: parentsData["root"].conference.praktiskTitle,
+});
 
 export default function Praktisk() {
+  const data = useRootData();
+
   return (
-    <article>
-      <Title as="h1" withBackground>
-        Om CapraCon
+    <ContentBox>
+      <Title as="h1" withBackground size="text-6xl">
+        {data.conference.praktiskTitle}
       </Title>
-      <Title as="h2">Velkommen til CapraCon 2023!</Title>
-    </article>
+      <p className="text-3xl font-bold">{data.conference.praktiskSubheading}</p>
+
+      {data.conference.praktiskDescription.split("\n\n").map((x, i) => (
+        <p className="mt-3 text-xl" key={i}>
+          {x}
+        </p>
+      ))}
+
+      <Title as="h2" withBackground size="text-6xl">
+        {data.conference.locationTitle}
+      </Title>
+
+      <dl className="text-xl [&>dt]:font-bold">
+        <dt>Lokasjon:</dt>
+        <dd>{data.conference.locationName}</dd>
+        <dt>Adresse:</dt>
+        <dd>{data.conference.locationAddress}</dd>
+        <dt>Hjemmeside:</dt>
+        <dd>{data.conference.locationHomepage}</dd>
+      </dl>
+    </ContentBox>
   );
 }
