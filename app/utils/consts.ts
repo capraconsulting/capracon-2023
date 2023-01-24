91a71f332c3853482078fdd0e794c0bbe614babb
Ingrid Seip Domben <ingrid.domben@gmail.com>
2023-01-24 18:49:24 +0100

    :lipstick: change track name

--- app/utils/consts.ts
+++ app/utils/consts.ts
@@ -1,27 +1,27 @@
 export enum Tracks {
   "Felles" = "Felles",
   "Frontend" = "Frontend",
-  "TPU" = "TPU",
+  "Team- prosjekt- og utviklingsledelse" = "Team- prosjekt- og utviklingsledelse",
   "CloudNative" = "CloudNative",
 }
 
 export const TRACKS = [
   Tracks.Felles,
   Tracks.Frontend,
-  Tracks.TPU,
+  Tracks["Team- prosjekt- og utviklingsledelse"],
   Tracks.CloudNative,
 ] as const;
 
 export const TrackGridColumn: Record<Tracks, string> = {
   [Tracks.Felles]: "track1 / track4",
   [Tracks.Frontend]: "track1",
-  [Tracks.TPU]: "track2",
+  [Tracks["Team- prosjekt- og utviklingsledelse"]]: "track2",
   [Tracks.CloudNative]: "track3",
 };
 
 // Which tracks to show headings for
 export const TRACK_HEADINGS = [
   Tracks.Frontend,
-  Tracks.TPU,
+  Tracks["Team- prosjekt- og utviklingsledelse"],
   Tracks.CloudNative,
 ] as const;
