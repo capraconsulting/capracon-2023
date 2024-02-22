c6b91788bfd7b83da101c531923e86191ca69f6f
Jakob Lund Johannessen <jakob-lj@hotmail.com>
2024-02-22 13:32:54 +0100

    feat: use 2024 databases

--- app/utils/consts.ts
+++ app/utils/consts.ts
@@ -3,27 +3,29 @@ export const timeZone = "Europe/Oslo";
 export enum Tracks {
   "Felles" = "Felles",
   "Frontend" = "Frontend",
-  "Ledelse" = "Ledelse",
-  "CloudNative" = "CloudNative",
+  "Ledelse" = "Teknologiledelse",
+  "Cloud" = "Cloud / Backend / Plattform",
 }
 
 export const TRACKS = [
   Tracks.Felles,
   Tracks.Frontend,
   Tracks.Ledelse,
-  Tracks.CloudNative,
+  Tracks.Cloud,
 ] as const;
 
 export const TrackGridColumn: Record<Tracks, string> = {
   [Tracks.Felles]: "track1 / track4",
   [Tracks.Frontend]: "track1",
   [Tracks.Ledelse]: "track2",
-  [Tracks.CloudNative]: "track3",
+  [Tracks.Cloud]: "track3",
 };
 
 // Which tracks to show headings for
 export const TRACK_HEADINGS = [
   Tracks.Frontend,
   Tracks.Ledelse,
-  Tracks.CloudNative,
+  Tracks.Cloud,
 ] as const;
+
+export const YEARS = ["2023", "2024"] as const;
