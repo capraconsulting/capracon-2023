ee150cd8c55c38af3ca70a8a8148909bd4526355
Jakob Lund Johannessen <jakob-lj@hotmail.com>
2024-02-29 15:28:57 +0100

    feat: use inter font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['"Inter"', ...defaultTheme.fontFamily.sans],
+      },
       colors: {
         frontend: "#bbdde6",
         ledelse: "#651d32",
