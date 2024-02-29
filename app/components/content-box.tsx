84472a7af16dd4a210785057fb05a17befdcfaff
Jakob Lund Johannessen <jakob-lj@hotmail.com>
2024-02-29 22:07:26 +0100

    feat: redesign content box

--- app/components/content-box.tsx
+++ app/components/content-box.tsx
@@ -5,7 +5,7 @@ export const ContentBox: React.FC<
 > = ({ children, ...rest }) => {
   return (
     <article
-      className="container mx-auto bg-primary-light px-4 pt-12 pb-24 shadow-md tablet:my-12 tablet:px-12 laptop:max-w-5xl"
+      className="container mx-auto px-4 pt-12 pb-24 tablet:my-12 tablet:px-12 laptop:max-w-5xl"
       {...rest}
     >
       {children}
