2f24258d39509de6b8331106b4efe5c54facdb7a
Anton Lilleby <antonlilleby@Anton-sin-MacBook-Pro.local>
2025-02-21 12:30:37 +0100

    fix: update mask with capracon

--- app/components/illustration-banner.tsx
+++ app/components/illustration-banner.tsx
@@ -1,12 +1,12 @@
 export const IllustrationBanner = () => {
   return (
-    <div className="max-h-[200px] min-h-[200px] w-full overflow-clip bg-[#09090B] [mask-image:url(/mask-sm.svg)] [mask-position:left_center] [mask-repeat:no-repeat] [mask-size:80%] tablet:[mask-image:url(/mask.svg)] tablet:[mask-size:50%] desktop:mt-24 dark:bg-white">
+    <div className="w-full overflow-clip bg-[#09090B] [mask-image:url(/mask.svg)] [mask-position:left_center] [mask-repeat:no-repeat] [mask-size:250px] tablet:mt-16  tablet:[mask-size:400px] desktop:[mask-size:500px] dark:bg-white">
       <picture className="motion-reduce:hidden">
         <source media="(min-width: 640px)" srcSet="/logo.webp" />
         <img
           alt="Bakgrunnsfarger til logo"
           src="/logo-sm.webp"
-          className="pointer-events-none -ml-[60px] max-h-[200px] min-h-[200px] w-[2100px] max-w-none object-cover blur-2xl contrast-125 tablet:-ml-[100px] tablet:w-[6000px] "
+          className="pointer-events-none -ml-[60px] h-[100px] w-[2100px] max-w-none object-cover blur-2xl contrast-125 tablet:-ml-[100px] tablet:h-[142px] tablet:w-[6000px] "
         />
       </picture>
     </div>
